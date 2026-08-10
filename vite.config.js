2b1b36deba83233cc4b62a4e6f609240ebebc249
James <james.alzate@gmail.com>
2026-08-10 02:23:29 -0500

    change .json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Disable React Compiler by using classic React plugin
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
